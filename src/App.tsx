dea7ec3f162d71fc2a942621ca10eae210dff78e
Jefferson M Souza <jefferson.msouza86@gmail.com>
2021-09-04 22:15:45 -0300

    adding todo to page signal

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import type { Component } from "solid-js";
+import { Component, createSignal } from 'solid-js';
+import { For } from 'solid-js';
 
-import logo from "./logo.svg";
-import styles from "./App.module.css";
+import styles from './App.module.css';
+
 
 const App: Component = () => {
+  const [todos, setTodos] = createSignal<string[]>([])
+
+  const addTodo = (input) => {
+    if (!input.value.trim()) return;
+    setTodos(val => val.concat(input.value))
+    input.value = '';
+  };
+  
+  let input: HTMLInputElement;
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <h2>TO-DO List App</h2>
       </header>
+      <main>
+        <label htmlFor="todo">New To-Do</label>
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          ref={input}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              addTodo(input);
+            }
+          }}
+        />
+        <ul>
+          <For each={todos()}>
+            {(item) => (
+              <li>
+                <input
+                  type="checkbox"
+                  name={item}
+                  //onClick={() => toggleTodo(item.text)}
+                />
+                <label for={item}>{item}</label>
+              </li>
+            )}
+          </For>
+        </ul>
+      </main>
     </div>
   );
 };
